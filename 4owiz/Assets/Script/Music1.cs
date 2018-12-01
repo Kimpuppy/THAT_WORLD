131dd5ab42ee7b199683846b842e8877ae247811
Kim Woojin <bakak112@naver.com>
2018-12-02 01:38:46 +0900

    Update Music1.cs

--- 4owiz/Assets/Script/Music1.cs
+++ 4owiz/Assets/Script/Music1.cs
@@ -6,22 +6,55 @@ public class Music1 : Music.BaseMusicSheet
 {
     public override void Setup()
     {
-        System.Func<Music.Pattern> test1 = () =>
+        System.Func<Music.Pattern> Blue = () =>
         {
             return new Music.Pattern(new List<Music.Note>{
                 new Music.Note(2, Music.NoteType.One, Music.CheckType.Blue, Music.GenPos.Left)
             });
         };
-        System.Func<Music.Pattern> test3 = () =>
+        System.Func<Music.Pattern> Green = () =>
         {
             return new Music.Pattern(new List<Music.Note>{
-				new Music.Note(2, Music.NoteType.One, Music.CheckType.Green, Music.GenPos.Down)
+                new Music.Note(2, Music.NoteType.One, Music.CheckType.Green, Music.GenPos.Down)
             });
         };
-        System.Func<Music.Pattern> test5 = () =>
+        System.Func<Music.Pattern> Red = () =>
         {
             return new Music.Pattern(new List<Music.Note>{
-				new Music.Note(2, Music.NoteType.One, Music.CheckType.Red, Music.GenPos.Right)
+                new Music.Note(2, Music.NoteType.One, Music.CheckType.Red, Music.GenPos.Right)
+            });
+        };
+
+        System.Func<Music.Pattern> BlueGreen = () =>
+        {
+            return new Music.Pattern(new List<Music.Note>{
+                new Music.Note(2, Music.NoteType.One, Music.CheckType.Blue, Music.GenPos.Left),
+                new Music.Note(2, Music.NoteType.One, Music.CheckType.Green, Music.GenPos.Left)
+            });
+        };
+
+        System.Func<Music.Pattern> BlueRed = () =>
+        {
+            return new Music.Pattern(new List<Music.Note>{
+                new Music.Note(2, Music.NoteType.One, Music.CheckType.Blue, Music.GenPos.Left),
+                new Music.Note(2, Music.NoteType.One, Music.CheckType.Green, Music.GenPos.Left)
+            });
+        };
+
+        System.Func<Music.Pattern> GreenRed = () =>
+        {
+            return new Music.Pattern(new List<Music.Note>{
+                new Music.Note(2, Music.NoteType.One, Music.CheckType.Blue, Music.GenPos.Left),
+                new Music.Note(2, Music.NoteType.One, Music.CheckType.Red, Music.GenPos.Left)
+            });
+        };
+
+        System.Func<Music.Pattern> BlueGreenRed = () =>
+        {
+            return new Music.Pattern(new List<Music.Note>{
+                new Music.Note(2, Music.NoteType.One, Music.CheckType.Blue, Music.GenPos.Left),
+                new Music.Note(2, Music.NoteType.One, Music.CheckType.Green, Music.GenPos.Left),
+                new Music.Note(2, Music.NoteType.One, Music.CheckType.Red, Music.GenPos.Left)
             });
         };
 
@@ -30,26 +63,9 @@ public class Music1 : Music.BaseMusicSheet
         _sheet3 = new List<Music.Action>();
 
         /// 노가다 해야함 ㅠㅠㅠ
-		_sheet1.Add(new Music.Action(4, test1()));
-        _sheet1.Add(new Music.Action(8, test1()));
-        _sheet1.Add(new Music.Action(12, test1()));
-        _sheet1.Add(new Music.Action(16, test1()));
-        _sheet1.Add(new Music.Action(20, test1()));
-        _sheet1.Add(new Music.Action(24, test1()));
-        _sheet1.Add(new Music.Action(28, test1()));
-        _sheet1.Add(new Music.Action(32, test1()));
-        _sheet1.Add(new Music.Action(36, test1()));
-        _sheet1.Add(new Music.Action(40, test1()));
-        _sheet2.Add(new Music.Action(16, test3()));
-        _sheet2.Add(new Music.Action(20, test3()));
-        _sheet2.Add(new Music.Action(24, test3()));
-        _sheet2.Add(new Music.Action(28, test3()));
-        _sheet2.Add(new Music.Action(32, test3()));
-        _sheet2.Add(new Music.Action(36, test3()));
-        _sheet3.Add(new Music.Action(24, test5()));
-        _sheet3.Add(new Music.Action(28, test5()));
-        _sheet3.Add(new Music.Action(32, test5()));
-        _sheet3.Add(new Music.Action(36, test5()));
+        _sheet1.Add(new Music.Action(4, Blue()));
+        _sheet1.Add(new Music.Action(6, Blue()));
+        _sheet1.Add(new Music.Action(7, Blue()));
 
         _bpm = 160.0f;
     }
